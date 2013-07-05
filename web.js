09f421a7c5d701491f8632b360cd45af46dbc71b
Vladislav Kuzmin <vladdyslav@gmail.com>
2013-07-05 12:46:22 +0000

    HW3 Part1

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+function readFromFile(fileName) {
+    var fileContents = fs.readFileSync(fileName, {encoding: "utf8"});
+    return fileContents;
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World Test!');
+  var buffer = new Buffer(readFromFile('index.html'), 'utf8');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
