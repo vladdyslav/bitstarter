fb79a6b57ea9ef7384b3e8a34f0640d65bd4fe2d
Vladislav Kuzmin <vladdyslav@gmail.com>
2013-07-05 12:50:57 +0000

    HW3 Part1 (fix encoding)

--- web.js
+++ web.js
@@ -4,12 +4,12 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 function readFromFile(fileName) {
-    var fileContents = fs.readFileSync(fileName, {encoding: "utf8"});
+    var fileContents = fs.readFileSync(fileName);
     return fileContents;
 }
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(readFromFile('index.html'), 'utf8');
+  var buffer = new Buffer(readFromFile('index.html'));
   response.send(buffer.toString());
 });
 
